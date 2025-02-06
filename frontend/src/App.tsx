657d63433444e72d01aeeeb441da2a33d0e0bf11
SIrGladkov <sir.gladkov@mail.ru>
2025-02-06 16:24:41 +0300

    AuthContext.tsx added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
+import ReactLoading from "react-loading";
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
+import { useAuthContext } from "./context/AutchContext"
 
 function App() {
+  const {authUser, isLoading} = useAuthContext();
+  
+if(isLoading){
+  return <div className="p-4 h-screen flex items-center justify-center"><ReactLoading
+  type="spinningBubbles"
+  color= "#00b300"
+  height={'5%'}
+  width={'5%'}
+  
+/></div>
+}
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/signup" element={<SignUp />}/>
-        <Route path="/login" element={<Login />}/>
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"}/>}/>
+        <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to={"/"}/>}/>
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to={"/"}/>}/>
       </Routes>
     </div>
   )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/AutchContext.tsx'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
