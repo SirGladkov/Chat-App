72ffa05aee9b9149c938b4649be556a18789096a
SIrGladkov <sir.gladkov@mail.ru>
2025-02-09 18:14:21 +0300

    message functionalitites added

---
+++ frontend/src/types/global.d.ts
@@ -0,0 +1,15 @@
+//Добавленные здесь типы могут использоваться в проекте глобально без их импорта.
+
+type ConversationType = {
+    id: string;
+    fullName: string;
+    profilePic: string;
+}
+
+type MessageType = {
+    id: string;
+    body: string;
+    senderId: string;
+    createdAt: string;
+    // анимация 
+}
